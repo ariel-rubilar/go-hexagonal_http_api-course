f1cfe7f721e0c4fbfec4029e3682532fa025825a
Ariel Rubilar <ariel.rubilar@capitalexpress.cl>
2025-04-21 18:26:52 -0400

    feat:Add initial implementation of the HTTP server with health check endpoint

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const httpAddr = ":8080"
+
+func main() {
+	fmt.Println("Starting server on", httpAddr)
+
+	srv := gin.New()
+	srv.GET("/health", healthHandler)
+
+	log.Fatal(srv.Run(httpAddr))
+}
+
+func healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
