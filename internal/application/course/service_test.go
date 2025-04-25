4740a265de56c7c073fd2696cd139cf63990b48d
Ariel Rubilar <ariel.rubilar@capitalexpress.cl>
2025-04-25 16:34:14 -0400

    feat: Add tests for CourseService Create method to validate error handling and success scenarios

---
+++ internal/application/course/service_test.go
@@ -0,0 +1,61 @@
+package course_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/ariel-rubilar/go-hexagonal_http_api-course/internal/application/course"
+	"github.com/ariel-rubilar/go-hexagonal_http_api-course/internal/domain/mooc"
+	"github.com/ariel-rubilar/go-hexagonal_http_api-course/test/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCourseService_Create_Fail(t *testing.T) {
+
+	mockCourseRepository := new(mocks.CourseRepositoryMock)
+	courseService := course.NewCourseService(mockCourseRepository)
+
+	t.Run("if course repository fails should return error", func(t *testing.T) {
+		mockCourseRepository.On("Save", mock.Anything, mock.Anything).Return(errors.New("error"))
+
+		id, name, duration := "123e4567-e89b-12d3-a456-426614174000", "Go Programming", "3 months"
+
+		_, err := courseService.Create(context.Background(), id, name, duration)
+
+		mockCourseRepository.AssertExpectations(t)
+		assert.Error(t, err)
+	})
+
+	t.Run("if new course fails should return error", func(t *testing.T) {
+
+		id, name, duration := "123e4567-e89b-12d3-a456-426614174000", "", "3 months"
+
+		_, err := courseService.Create(context.Background(), id, name, duration)
+
+		mockCourseRepository.AssertNotCalled(t, "Save", mock.Anything, mock.Anything)
+		assert.Error(t, err)
+	})
+}
+
+func TestCourseService_Create_Success(t *testing.T) {
+
+	mockCourseRepository := new(mocks.CourseRepositoryMock)
+	courseService := course.NewCourseService(mockCourseRepository)
+
+	id, name, duration := "123e4567-e89b-12d3-a456-426614174000", "Go Programming", "3 months"
+
+	course, err := mooc.NewCourse(id, name, duration)
+
+	require.NoError(t, err)
+
+	mockCourseRepository.On("Save", mock.Anything, course).Return(nil)
+
+	newCourse, err := courseService.Create(context.Background(), id, name, duration)
+
+	assert.NoError(t, err)
+	assert.Equal(t, course, newCourse)
+
+}
