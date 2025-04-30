72853e24479e7b2556ec4d7bf1187d029954c828
Ariel Rubilar <ariel.rubilar@capitalexpress.cl>
2025-04-30 11:50:23 -0400

    refactor: Update server Run method to use http.Server for improved handling

--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -3,6 +3,11 @@ package server
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ariel-rubilar/go-hexagonal_http_api-course/internal/platform/server/handler/courses"
 	"github.com/ariel-rubilar/go-hexagonal_http_api-course/internal/platform/server/handler/health"
@@ -30,12 +35,28 @@ func New(ctx context.Context, host string, port int, b command.Bus) (context.Con
 
 	api.registerRoutes()
 
-	return ctx, api
+	return serverContext(ctx), api
 }
 
 func (a *api) Run(ctx context.Context) error {
 	fmt.Println("Starting server on", a.httpAddr)
-	return a.engine.Run(a.httpAddr)
+
+	srv := &http.Server{
+		Addr:    a.httpAddr,
+		Handler: a.engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return srv.Shutdown(ctx)
 }
 
 func (a *api) registerRoutes() {
@@ -43,4 +64,18 @@ func (a *api) registerRoutes() {
 	a.engine.GET("/health", health.CheckHandler())
 	a.engine.POST("/courses", courses.CreateHandler(a.bus))
 	a.engine.GET("/courses", courses.ListHandler(a.bus))
+}
+
+func serverContext(ctx context.Context) context.Context {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		<-c
+		cancel()
+	}()
+
+	return ctx
 }
